62d3df6117ad98b2a40a21edbbd6da6d6589d40b
dhana2221 <dhanatirumalasetti@gmail.com>
2025-08-29 00:33:31 -0400

    decode_image

--- utils.py
+++ utils.py
@@ -5,3 +5,9 @@ def encode_image(path: str) -> str:
         with open(path, "rb") as f:
          return base64.b64encode(f.read()).decode("utf-8")
 
+
+
+# def decode_image(b64_string: str, output_path: str) -> None:
+#     """Decode a Base64 string and save it as an image file."""
+#     with open(output_path, "wb") as f:
+#         f.write(base64.b64decode(b64_string))
